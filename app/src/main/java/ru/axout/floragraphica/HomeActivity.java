f445441471b4fba98ed9706fc7074a4008ea4eb2
Axout <rako-serge@ya.ru>
2020-12-03 00:31:22 +0300

    Добавлены три активити-площадки В HomeActivity появились три новые ImageView, по нажатию на которые открываются новые активити.

--- app/src/main/java/ru/axout/floragraphica/HomeActivity.java
+++ app/src/main/java/ru/axout/floragraphica/HomeActivity.java
@@ -1,14 +1,46 @@
 package ru.axout.floragraphica;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageView imTula, imFood, imVarsh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        imTula = findViewById(R.id.im_Tula);
+        imFood = findViewById(R.id.im_Food);
+        imVarsh = findViewById(R.id.im_Varsh);
+
+        imTula.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, TulaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imVarsh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, VarshActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/ru/axout/floragraphica/TulaActivity.java
@@ -0,0 +1,13 @@
+package ru.axout.floragraphica;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TulaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tula);
+    }
+}
